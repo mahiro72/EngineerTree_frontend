b765c5af60c0021866f440bff3e885fc9667e0f8
mahiro <mahiro.yoshie@gmail.com>
2021-11-09 22:14:35 +0900

    tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -22,4 +22,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
